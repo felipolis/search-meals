8fb510c6d33433c022f77b05fa4a509e2c897872
Felipe Archanjo <felipecunhamendes@gmail.com>
2023-07-13 00:48:07 -0300

    Criando a página Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,23 @@
 <template>
-    <pre>{{ meals }}</pre>
+  <div class="flex flex-col p-8">
+    <div>
+      <input
+        type="text"
+        class="rounded border-2 border-gray-200 w-full"
+        placeholder="Search For Meals"
+      />
+    </div>
+
+    <div class="flex justify-center gap-2 mt-2">
+      <router-link
+        v-for="letter in letters"
+        :key="letter"
+        :to="{name: 'byLetter', params: {letter}}"
+      >
+        {{ letter }}
+      </router-link>
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -7,4 +25,5 @@ import { computed } from "vue";
 import store from "../store";
 
 const meals = computed(() => store.state.meals);
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 </script>
