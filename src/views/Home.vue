311e4ac8d92d4e73f2c89595b955c3f69558ca34
Felipe Archanjo <felipecunhamendes@gmail.com>
2023-07-13 00:28:39 -0300

    Definindo inicialmente a store

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,10 @@
 <template>
-    <div>
-        
-    </div>
+    <pre>{{ meals }}</pre>
 </template>
 
 <script setup>
+import { computed } from "vue";
+import store from "../store";
 
+const meals = computed(() => store.state.meals);
 </script>
