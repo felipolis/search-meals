a6b22f9eae1cafd30369c13bf4b4d88cf4418ebc
Felipe Archanjo <felipecunhamendes@gmail.com>
2023-08-07 19:51:54 -0300

    Criação do layout da página

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -21,16 +21,19 @@
 </template>
 
 <script setup>
-import { computed, onMounted } from "vue";
+import { computed, onMounted, ref } from "vue";
 import store from "../store";
 import axiosClient from "../axiosClient";
 
 const meals = computed(() => store.state.meals);
+
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+const ingredients = ref([]);
 
 onMounted(async () => {
   const response = await axiosClient.get("/list.php?i=list")
 
   console.log(response.data);
+  ingredients.value = response.data;
 });
 </script>
