502ba47153b475bee559533443f500694517c082
Felipe Archanjo <felipecunhamendes@gmail.com>
2023-08-08 00:08:14 -0300

    Realizando requisição de meals pelo nome

--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -1,8 +1,32 @@
 <template>
+    <div class="p-8">
+      <input
+        type="text"
+        v-model="keyword"
+        class="rounded border-2 border-gray-200 w-full"
+        placeholder="Search For Meals"
+        @change="searchMeals"
+      />
+    </div>
+
     <div>
-      By Name
+      <pre>
+        {{ meals }}
+      </pre>
     </div>
   </template>
   
-  <script setup></script>
+<script setup>
+  import { ref } from "vue";
+  import { computed } from '@vue/reactivity'
+  import store from "../store";
+
+  const keyword = ref('');
+  const meals = computed(() => store.state.searchedMeals)
+
+  function searchMeals() {
+    store.dispatch("searchMeals", keyword.value);
+  }
+
+</script>
   
