06326499b6a2e8eeba100b3c70c0b54dea28667d
Felipe Archanjo <felipecunhamendes@gmail.com>
2023-08-08 18:56:56 -0300

    Criando card das meals

--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -1,32 +1,55 @@
 <template>
-    <div class="p-8">
-      <input
-        type="text"
-        v-model="keyword"
-        class="rounded border-2 border-gray-200 w-full"
-        placeholder="Search For Meals"
-        @change="searchMeals"
+  <div class="p-8">
+    <input
+      type="text"
+      v-model="keyword"
+      class="rounded border-2 border-gray-200 w-full"
+      placeholder="Search For Meals"
+      @change="searchMeals"
+    />
+  </div>
+
+  <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
+    <div
+      v-for="meal of meals"
+      :key="meal.idMeal"
+      class="bg-white shadow rounded-xl"
+    >
+      <img
+        :src="meal.strMealThumb"
+        alt="strMeal"
+        class="rounded-t-xl h-48 w-full object-cover"
       />
+      <div class="p-3">
+        <h3 class="font-bold">{{ meal.strMeal }}</h3>
+        <p class="mb-4">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
+          deleniti, vitae dolorem mollitia ea impedit, molestias officiis
+          aspernatur rerum earum quis culpa est. Ab dolorem rem perspiciatis
+          numquam. Nulla, commodi?
+        </p>
+        <div class="flex items-center justify-between">
+          <a
+            :href="meal.strYoutube"
+            target="_blank"
+            class="px-3 py-2 text-white rounded border-2 border-red-600 bg-red-500 hover:bg-red-600 transition-colors"
+            >Youtube</a
+          >
+        </div>
+      </div>
     </div>
+  </div>
+</template>
 
-    <div>
-      <pre>
-        {{ meals }}
-      </pre>
-    </div>
-  </template>
-  
 <script setup>
-  import { ref } from "vue";
-  import { computed } from '@vue/reactivity'
-  import store from "../store";
+import { ref } from "vue";
+import { computed } from "@vue/reactivity";
+import store from "../store";
 
-  const keyword = ref('');
-  const meals = computed(() => store.state.searchedMeals)
+const keyword = ref("");
+const meals = computed(() => store.state.searchedMeals);
 
-  function searchMeals() {
-    store.dispatch("searchMeals", keyword.value);
-  }
-
+function searchMeals() {
+  store.dispatch("searchMeals", keyword.value);
+}
 </script>
-  
