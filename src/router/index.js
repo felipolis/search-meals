ae81569a8bb0db08e5888e34449e53fd5c341aaf
Felipe Archanjo <felipecunhamendes@gmail.com>
2023-08-08 19:35:24 -0300

    Criando a pagina de meal

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import MealsByName from "../views/MealsByName.vue";
 
 import DefaultLayout from "../components/DefaultLayout.vue";
 import GuestLayout from "../components/GuestLayout.vue";
+import MealDetails from "../views/MealDetails.vue";
 
 const routes = [
   {
@@ -31,6 +32,11 @@ const routes = [
         path: "/by-ingredient/:ingredient?",
         name: "byIngredient",
         component: MealsByIngredient
+      },
+      {
+        path: "/meal/:id",
+        name: "mealDetails",
+        component: MealDetails
       }
     ]
   },
