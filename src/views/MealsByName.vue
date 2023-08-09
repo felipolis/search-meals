df2174c8e91b4ed53b83a2e684b423d2ea92ab72
Felipe Archanjo <felipecunhamendes@gmail.com>
2023-08-09 00:31:26 -0300

    Melhorando css

--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -3,7 +3,7 @@
     <input
       type="text"
       v-model="keyword"
-      class="rounded border-2 border-gray-200 w-full"
+      class="rounded border-2 bg-white border-gray-200 w-full"
       placeholder="Search For Meals"
       @change="searchMeals"
     />
@@ -27,7 +27,11 @@ const keyword = ref("");
 const meals = computed(() => store.state.searchedMeals);
 
 function searchMeals() {
-  store.dispatch("searchMeals", keyword.value);
+  if (keyword.value) {
+    store.dispatch("searchMeals", keyword.value);
+  } else {
+    store.commit("setSearchedMeals", []);
+  }
 }
 
 onMounted(() => {
