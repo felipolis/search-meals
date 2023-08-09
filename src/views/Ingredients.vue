8498384b2c8362a77939a98b52f3cd230700cf3a
Felipe Archanjo <felipecunhamendes@gmail.com>
2023-08-09 15:14:46 -0300

    Arrumando pagina de ingredientes

--- src/views/Ingredients.vue
+++ src/views/Ingredients.vue
@@ -1,31 +1,44 @@
 <template>
-    <div class="p-8">
-      <h1 class="text-4xl font-bold mb-4">Ingredients</h1>
-      <router-link
-        :to="{
-          name: 'byIngredient',
-          params: { ingredient: ingredient.strIngredient },
-        }"
-        v-for="ingredient of ingredients"
-        :key="ingredient.idIngredient"
-        class="block bg-white rounded p-3 mb-3 shadow"
-      >
-        <h3 class="text2xl font-bold mb-2">{{ ingredient.strIngredient }}</h3>
-        <p>{{ ingredient.strDescription }}</p>
-      </router-link>
-    </div>
-  </template>
-  
-  <script setup>
-  import { onMounted, ref } from "vue";
-  import axiosClient from "../axiosClient";
-  
-  const ingredients = ref([]);
-  
-  onMounted(() => {
-    axiosClient.get("list.php?i=list").then(({ data }) => {
-      ingredients.value = data.meals;
-    });
+  <div class="p-8">
+    <h1 class="text-4xl font-bold mb-4">Ingredients</h1>
+    <input
+      type="text"
+      v-model="keyword"
+      class="rounded border-2 bg-white border-gray-200 w-full mb-3"
+      placeholder="Search For Ingredients"
+    />
+    <router-link
+      :to="{
+        name: 'byIngredient',
+        params: { ingredient: ingredient.strIngredient },
+      }"
+      v-for="ingredient of computedIngredients"
+      :key="ingredient.idIngredient"
+      class="block bg-white rounded p-3 mb-3 shadow"
+    >
+      <h3 class="text2xl font-bold mb-2">{{ ingredient.strIngredient }}</h3>
+      <p>{{ ingredient.strDescription }}</p>
+    </router-link>
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from "vue";
+import axiosClient from "../axiosClient";
+import { computed } from "@vue/reactivity";
+
+const ingredients = ref([]);
+const keyword = ref("");
+const computedIngredients = computed(() => {
+  if (!computedIngredients) return ingredients;
+  return ingredients.value.filter(i => 
+    i.strIngredient.toLowerCase().includes(keyword.value.toLowerCase())
+  )
+})
+
+onMounted(() => {
+  axiosClient.get("list.php?i=list").then(({ data }) => {
+    ingredients.value = data.meals;
   });
-  </script>
-  
+});
+</script>
